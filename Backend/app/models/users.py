a560cc52c093ce6523395b88b0b4ad4ada68fe59
kotharisoumil <kotharisoumil@gmail.com>
2026-04-08 14:53:27 -0400

    Users model created

---
+++ Backend/app/models/users.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from ..dependencies.database import Base
+
+
+class User(Base):
+  __tablename__ = "users"
+
+  id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+  username = Column(String(50),  unique=True, nullable=False, index=True)
+  email = Column(String(255), unique=True, nullable=False, index=True)
+  password_hash = Column(String(255), nullable=False)
+  full_name = Column(String(100), nullable=False)
+
+  memberships = relationship("GroupMember", back_populates="user")
+  chore_assignments = relationship("ChoreAssignment", back_populates="assigned_user")
+  expenses_paid = relationship("Expense", back_populates="paid_by_user")
+  expense_splits = relationship("ExpenseSplit", back_populates="user")
+  payments_made = relationship("Payment", foreign_keys="Payment.payer_id", back_populates="payer")
+  payments_received = relationship("Payment", foreign_keys="Payment.payee_id", back_populates="payee")
